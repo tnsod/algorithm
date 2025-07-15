0c355541be3295f58ed77a5d15ab67567ab987b9
tnsod <hek514814@naver.com>
2025-07-16 02:14:18 +0900

    Update main.py

--- BOJ/Gold_Random_Defence/1365 꼬인 전깃줄/main.py
+++ BOJ/Gold_Random_Defence/1365 꼬인 전깃줄/main.py
@@ -9,15 +9,19 @@ def lower_bound(arr,st,en,key):
         else:
             en=mid
     return en
-    
-N=int(input())
-arr=tuple(map(int,input().split()))
-lis=[arr[0]]
-for i in range(1,N):
-    if lis[-1]<arr[i]:
-        lis.append(arr[i])
-    else:
-        idx=lower_bound(lis,0,len(lis)-1,arr[i])
-        lis[idx]=arr[i]
 
-print(N-len(lis))
+def main():
+    N=int(input())
+    arr=tuple(map(int,input().split()))
+    lis=[arr[0]]
+    for i in range(1,N):
+        if lis[-1]<arr[i]:
+            lis.append(arr[i])
+        else:
+            idx=lower_bound(lis,0,len(lis)-1,arr[i])
+            lis[idx]=arr[i]
+
+    print(N-len(lis))
+
+if __name__=='__main__':
+    main()
